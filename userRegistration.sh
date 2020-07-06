95d261ac171b48f4b6b518be623527a4a86e893a
Nishit <1407162@EEE1407203.EEE1407203>
2020-07-06 21:06:35 +0530

    UC3

--- userRegistration.sh
+++ userRegistration.sh
@@ -6,9 +6,12 @@ echo "First Name"
 read firstN
 echo "Last Name"
 read lastN
+echo "Enter the Email ID"
+read email
 
 
 pat1="^([A-Z]{1}[a-z]{2,} {0,1})+$"
+pat2="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[0-9a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
 
 if [[ $firstN =~ $pat1 ]]
 then
@@ -20,4 +23,11 @@ then
     fi
 else
     echo "Invalid First Name"
+fi
+
+if [[ $email =~ $pat2 ]]
+then
+    echo "email :- "$email
+else
+    echo "Invalid User Email ID"
 fi
