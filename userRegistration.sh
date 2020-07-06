0cf5850a5b03a996980f7b6c553088934ff40fe0
Nishit <1407162@EEE1407203.EEE1407203>
2020-07-06 21:29:31 +0530

    UC2

--- userRegistration.sh
+++ userRegistration.sh
@@ -8,10 +8,17 @@ echo "Last Name"
 read lastN
 echo "Enter the Email ID"
 read email
+echo "Enter Your Mobile Number"
+read num
 
 
 pat1="^([A-Z]{1}[a-z]{2,} {0,1})+$"
+
 pat2="^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[0-9a-zA-Z]{2,4}([.][a-zA-Z]{2})*$"
+
+pat3="^([1-9]{1}([-][1-9]{1}[0-9]{1,2}||[0-9]{1,3})) {1}[1-9]{1}[0-9]{9}$"
+
+
 
 if [[ $firstN =~ $pat1 ]]
 then
@@ -30,4 +37,11 @@ then
     echo "email :- "$email
 else
     echo "Invalid User Email ID"
+fi
+
+if [[ $num =~ $pat3 ]]
+then
+    echo "Mobile no:- "$num
+else
+    echo "Invalid Mobile Number"
 fi
