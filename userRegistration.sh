669c681f6a6a5f3935f8e1d3cf876b244f7dd963
Nishit <1407162@EEE1407203.EEE1407203>
2020-07-06 21:03:10 +0530

    UC2

--- userRegistration.sh
+++ userRegistration.sh
@@ -4,14 +4,20 @@ shopt -s extglob
 
 echo "First Name"
 read firstN
+echo "Last Name"
+read lastN
 
 
 pat1="^([A-Z]{1}[a-z]{2,} {0,1})+$"
 
 if [[ $firstN =~ $pat1 ]]
 then
-    echo "Name :-"$firstN
+    if [[ $lastN =~ $pat1 ]]
+    then
+        echo "Name :- "$firstN" "$lastN
+    else
+        echo "Invalid Last Name"
+    fi
 else
     echo "Invalid First Name"
 fi
-
